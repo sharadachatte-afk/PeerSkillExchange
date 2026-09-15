ae3ab3426d1e2108b7c4db437e3852aac786cb76
sharda chatte <sharadachatte@gmail.com>
2026-09-15 12:28:47 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -96,7 +96,7 @@ profileForm.addEventListener(
         try {
 
             const response = await fetch(
-                `http://localhost:5000/api/users/${currentUser.id}`,
+                `/api/users/${currentUser.id}`,
                 {
                     method: "PUT",
 
