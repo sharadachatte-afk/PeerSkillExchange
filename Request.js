c5a700168123238eef515ba5befd88a171d439ba
sharda chatte <sharadachatte@gmail.com>
2026-09-15 13:46:52 +0530

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const requestSchema = new mongoose.Schema({
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
+const requestSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
 
-    receiver: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
 
-    status: {
-        type: String,
-        enum: ["pending", "accepted", "rejected"],
-        default: "pending"
+        status: {
+            type: String,
+            enum: ["pending", "accepted", "rejected"],
+            default: "pending"
+        }
+    },
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true
-});
+);
 
 module.exports = mongoose.model("Request", requestSchema);
